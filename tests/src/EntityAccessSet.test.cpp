914f655e31a820a468e4088a91cfb2032553445d
ThorstenSuckow <thorsten@suckow-homberg.de>
2026-08-10 19:57:46 +0200

    feat: add EntityAccessSet for providing static read/write access information

---
+++ tests/src/EntityAccessSet.test.cpp
@@ -0,0 +1,112 @@
+#include <gtest/gtest.h>
+#include "helios-ecs-config.h"
+
+import helios.ecs;
+
+using namespace helios::ecs;
+using namespace helios::ecs::types;
+
+
+namespace {
+
+
+    struct GameObjectHandle{};
+
+    struct ParticleHandle{};
+
+    template<typename TMOwnerHandle>
+    struct PositionComponent {
+        using Handle_type = TMOwnerHandle;
+    };
+
+    template<typename TMOwnerHandle>
+    struct VelocityComponent {
+        using Handle_type = TMOwnerHandle;
+    };
+
+
+    struct Foo {
+
+        using EntityAccessSet = EntityAccessSet<
+            Read<
+                PositionComponent<GameObjectHandle>,
+                VelocityComponent<ParticleHandle>,
+                VelocityComponent<GameObjectHandle>
+            >,
+            Write<VelocityComponent<GameObjectHandle>>
+        >;
+
+        EntityAccessSet entityAccessSet;
+    };
+};
+
+TEST(EntityAccessSet, Components) {
+
+    EXPECT_TRUE((
+        std::same_as<
+            helios::ecs::types::Read<
+                PositionComponent<GameObjectHandle>,VelocityComponent<ParticleHandle>,VelocityComponent<GameObjectHandle>
+            >,
+            Foo::EntityAccessSet::ReadComponentSet
+        >
+    ));
+
+    EXPECT_TRUE((
+        std::same_as<
+            helios::ecs::types::Write<
+                VelocityComponent<GameObjectHandle>
+            >,
+            Foo::EntityAccessSet::WriteComponentSet
+        >
+    ));
+}
+
+
+TEST(EntityAccessSet, tuples) {
+
+    EXPECT_EQ(2, (std::tuple_size_v<typename Foo::EntityAccessSet::ReadHandles>));
+    EXPECT_EQ(1, (std::tuple_size_v<typename Foo::EntityAccessSet::WriteHandles>));
+    EXPECT_EQ(2, (std::tuple_size_v<typename Foo::EntityAccessSet::AccessHandles>));
+
+
+    // READ
+    EXPECT_TRUE((
+        std::same_as<
+            GameObjectHandle,
+            std::tuple_element_t<0, typename Foo::EntityAccessSet::ReadHandles>
+        >
+    ));
+
+    EXPECT_TRUE((
+        std::same_as<
+            ParticleHandle,
+            std::tuple_element_t<1, typename Foo::EntityAccessSet::ReadHandles>
+        >
+    ));
+
+    // WRITE
+    EXPECT_TRUE((
+       std::same_as<
+           GameObjectHandle,
+           std::tuple_element_t<0, typename Foo::EntityAccessSet::WriteHandles>
+       >
+   ));
+
+
+    //ACCESS
+    EXPECT_TRUE((
+        std::same_as<
+            GameObjectHandle,
+            std::tuple_element_t<0, typename Foo::EntityAccessSet::AccessHandles>
+            >
+    ));
+
+    EXPECT_TRUE((
+        std::same_as<
+            ParticleHandle,
+            std::tuple_element_t<1, typename Foo::EntityAccessSet::AccessHandles>
+        >
+    ));
+
+
+}
